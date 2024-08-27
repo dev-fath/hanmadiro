f7a6d21cbb2a691f5d5cfdaf1455da38c452fe78
DEVFATH <ggje33@gmail.com>
2024-08-27 10:26:38 +0900

    skia 텍스트 렌더링 - paragraph 사용예시 추가

--- components/SkiaText.tsx
+++ components/SkiaText.tsx
@@ -9,7 +9,7 @@ import {
   Fill,
   useFonts,
   matchFont,
-  Paragraph
+  Paragraph, TextAlign,
 } from "@shopify/react-native-skia";
 import { useMemo } from "react";
 import { useWindowDimensions } from "react-native";
@@ -25,14 +25,42 @@ export const HelloWorld = () => {
   const p = useMemo(() => {
     const builder = Skia.ParagraphBuilder.Make();
     builder.addText("대충 길게 한번 찌끄려보는 한글을 더 길게 써보면 어떻게 되는지 한번 보고싶다");
+    builder.pushStyle({ fontFeatures: [], fontVariations: [], fontFamilies: ['Nanum-Kanginhan'], fontSize: fontSize, color: new Float32Array([0.0, 0.0, 1.0, 1.0])})
     return builder.build();
   }, []);
+  const customFontMgr = useFonts({
+    Kanginhan: [
+      require('../assets/fonts/Nanum-Kanginhan.ttf')
+    ]
+  });
 
+  const paragraph = useMemo(() => {
+    // Are the font loaded already?
+    if (!customFontMgr) {
+      return null;
+    }
+    const paragraphStyle = {
+      textAlign: TextAlign.Center
+    };
+    const textStyle = {
+      color: Skia.Color("black"),
+      fontFamilies: ["Kanginhan"],
+      fontSize: 50,
+    };
+    return Skia.ParagraphBuilder.Make(paragraphStyle, customFontMgr)
+      .pushStyle(textStyle)
+      .addText("대충 길게 한번 찌끄려보는 한글을 더 길게 써보면 어떻게 되는지 한번 보고싶다")
+      .addText("Say Hello to ")
+      .pushStyle({ ...textStyle, fontStyle: { weight: 500 } })
+      .addText("Skia 🎨")
+      .pop()
+      .build();
+  }, [customFontMgr]);
   return (
-      <Canvas style={{ flex: 1, width: '100%' }}>
+      <Canvas style={{ flex: 1, width: width }}>
         <Fill color="white" />
-        <Text x={10} y={50} font={font} text="대충 길게 한번 찌끄려보는 한글을 더 길게 써보면 어떻게 되는지 한번 보고싶다"></Text>
-        <Paragraph paragraph={p} width={width-48} x={0} y={0}r/>
+        <Paragraph paragraph={paragraph} width={width-48} x={10} y={10} />
+        {/*<Text x={10} y={50} font={font} text="대충 길게 한번 찌끄려보는 한글을 더 길게 써보면 어떻게 되는지 한번 보고싶다"></Text>*/}
         {/*<Group transform={[{ rotate: Math.PI }]} origin={vec(size, size)}>*/}
         {/*  <TextPath font={font} path={path} text="대충 길게 한번 찌끄려보는 한글을 더 길게 써보면 어떻게 되는지 한번 보고싶다" color={'black'} />*/}
         {/*</Group>*/}
